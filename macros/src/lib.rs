ea8dfd1e420e4b63dca56ba5f9b375709e25c0ac
Dustin J. Mitchell <dustin@v.igoro.us>
2024-07-09 08:40:28 -0400

    finish generation of visitor

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -7,9 +7,10 @@ use syn::{
     punctuated::Punctuated,
     spanned::Spanned,
     token::Comma,
-    AngleBracketedGenericArguments, Error, Expr, Field, FieldValue, Fields, FnArg, GenericArgument,
-    Ident, Item, ItemEnum, ItemStruct, Pat, PatIdent, PatType, Path, PathArguments, PathSegment,
-    Token, TraitBound, TraitBoundModifier, Type, TypeImplTrait, TypeParamBound,
+    AngleBracketedGenericArguments, Arm, Error, Expr, Field, FieldPat, FieldValue, Fields, FnArg,
+    GenericArgument, Ident, Item, ItemEnum, ItemStruct, Pat, PatIdent, PatType, Path,
+    PathArguments, PathSegment, Token, TraitBound, TraitBoundModifier, Type, TypeImplTrait,
+    TypeParamBound,
 };
 
 struct Items(Vec<Item>);
@@ -108,7 +109,7 @@ impl AstGenerator {
         let tokens = self.out_visitor_trait;
         result.extend(quote! {
             #[allow(dead_code)]
-            trait Visitor {
+            trait Visitor: Sized {
                 #tokens
             }
         });
@@ -140,11 +141,13 @@ impl AstGenerator {
 
         // Create a constructor.
         let mut methods = proc_macro2::TokenStream::new();
-        let Fields::Named(fields) = item.fields else {
+        let Fields::Named(named_fields) = &item.fields else {
+            // TODO: support tuple fields
             panic!("expected named fields");
         };
-        let args: Punctuated<FnArg, Comma> = fields.named.iter().map(fn_arg_impl_into).collect();
-        let long_init: Punctuated<FieldValue, Comma> = fields
+        let args: Punctuated<FnArg, Comma> =
+            named_fields.named.iter().map(fn_arg_impl_into).collect();
+        let long_init: Punctuated<FieldValue, Comma> = named_fields
             .named
             .iter()
             .map(|f| {
@@ -164,10 +167,21 @@ impl AstGenerator {
         self.out_type_impls.insert(struct_name.clone(), methods);
 
         self.add_visitor_pre(struct_name)?;
+
+        // Create a <type>_recurse fn.
+        let lower_name = struct_name.to_string().to_lowercase();
+        let recurse = Ident::new(&format!("{lower_name}_recurse"), struct_name.span());
+        let recurse_body = recurse_for_fields(&item.fields, |ident| quote! { (&mut val.#ident) })?;
+        self.out_visitor_trait.extend(quote! {
+            #[inline]
+            fn #recurse(&mut self, val: &mut #struct_name) -> Result<()> {
+                #recurse_body
+                Ok(())
+            }
+        });
+
         self.add_visitor_post(struct_name)?;
         self.add_node_type_impl(struct_name)?;
-
-        // TODO: visitor recurse fn
 
         Ok(())
     }
@@ -183,11 +197,16 @@ impl AstGenerator {
             },
         );
 
-        // Create a constructor for each variant, returning `Node<K>`.
+        // Create a constructor for each variant, returning `Node<K>`, and a `K_recurse` Visitor
+        // method.
         let mut methods = proc_macro2::TokenStream::new();
+        let mut recurse_match_arms = proc_macro2::TokenStream::new();
+
         for variant in &item.variants {
             let var_name = &variant.ident;
             let constr_name = lowercase(var_name);
+
+            let recurse_body = recurse_for_fields(&variant.fields, |ident| quote! { #ident })?;
 
             match &variant.fields {
                 Fields::Named(fields) => {
@@ -210,9 +229,21 @@ impl AstGenerator {
                             }
                         }
                     });
+                    if !recurse_body.is_empty() {
+                        let field_pats: Punctuated<PatIdent, Comma> = fields
+                            .named
+                            .iter()
+                            .map(|f| pat_ident(f.ident.as_ref().unwrap().clone()))
+                            .collect();
+                        recurse_match_arms.extend(quote! {
+                            #enum_name::#var_name { #field_pats } => {
+                                #recurse_body
+                            },
+                        });
+                    }
                 }
                 Fields::Unnamed(fields) => {
-                    let args: Punctuated<FnArg, Comma> = fields
+                    let constr_args: Punctuated<FnArg, Comma> = fields
                         .unnamed
                         .iter()
                         .enumerate()
@@ -222,7 +253,7 @@ impl AstGenerator {
                             fn_arg_impl_into(&f)
                         })
                         .collect();
-                    let fields: Punctuated<Expr, Comma> = fields
+                    let constr_fields: Punctuated<Expr, Comma> = fields
                         .unnamed
                         .iter()
                         .enumerate()
@@ -230,13 +261,26 @@ impl AstGenerator {
                         .collect();
                     methods.extend(quote! {
                         #[inline]
-                        #vis fn #constr_name(src: Src, #args) -> Node<#enum_name> {
+                        #vis fn #constr_name(src: Src, #constr_args) -> Node<#enum_name> {
                             Node {
                                 src,
-                                inner: #enum_name::#var_name(#fields),
+                                inner: #enum_name::#var_name(#constr_fields),
                             }
                         }
                     });
+                    if !recurse_body.is_empty() {
+                        let match_args: Punctuated<PatIdent, Comma> = fields
+                            .unnamed
+                            .iter()
+                            .enumerate()
+                            .map(|(i, f)| pat_ident(Ident::new(&format!("v{i}"), f.ident.span())))
+                            .collect();
+                        recurse_match_arms.extend(quote! {
+                            #enum_name::#var_name ( #match_args ) => {
+                                #recurse_body
+                            },
+                        });
+                    }
                 }
                 Fields::Unit => {
                     methods.extend(quote! {
@@ -253,50 +297,23 @@ impl AstGenerator {
         }
         self.out_type_impls.insert(enum_name.clone(), methods);
 
-        // Build the foo_recurse function
-        let mut match_body = proc_macro2::TokenStream::new();
-
-        for variant in item.variants {
-            let var_name = &variant.ident;
-            match &variant.fields {
-                Fields::Named(fields) => {
-                    todo!(); // TODO
-                }
-                Fields::Unnamed(fields) => {
-                    let args: Punctuated<Ident, Comma> = fields
-                        .unnamed
-                        .iter()
-                        .enumerate()
-                        .map(|(i, f)| Ident::new(&format!("v{i}"), f.ident.span()))
-                        .collect();
-                    match_body.extend(quote! {
-                        // TODO: traverse the Node/NodeRef values.
-                        #var_name ( #args ) => todo!(),
-                    });
-                }
-                Fields::Unit => {}
-            };
-        }
+        self.add_visitor_pre(enum_name)?;
         let lower_name = enum_name.to_string().to_lowercase();
         let recurse = Ident::new(&format!("{lower_name}_recurse"), enum_name.span());
-        self.out_visitor_trait
-            .extend(quote! {
-                #[inline]
-                fn #recurse(&mut self, val: &mut #enum_name) -> Result<()> {
-                    #[allow(unused_variables, unreachable_patterns)]
-                    match val {
-                        #match_body
-                        _ => {}
-                    };
-                    Ok(())
-                }
-            });
-
-        self.add_visitor_pre(enum_name)?;
+        self.out_visitor_trait.extend(quote! {
+            #[inline]
+            fn #recurse(&mut self, val: &mut #enum_name) -> crate::error::Result<()> {
+                #[allow(unused_variables, unreachable_patterns)]
+                match val {
+                    #recurse_match_arms
+                    _ => {}
+                };
+                Ok(())
+            }
+        });
         self.add_visitor_post(enum_name)?;
-        self.add_node_type_impl(enum_name)?;
 
-        // TODO: visitor recurse fn
+        self.add_node_type_impl(enum_name)?;
 
         Ok(())
     }
@@ -304,26 +321,24 @@ impl AstGenerator {
     fn add_visitor_pre(&mut self, name: &Ident) -> syn::Result<()> {
         let lower_name = name.to_string().to_lowercase();
         let pre = Ident::new(&format!("{lower_name}_pre"), name.span());
-        self.out_visitor_trait
-            .extend(quote! {
-                #[inline]
-                fn #pre(&mut self, val: &mut #name) -> Result<bool> {
-                    Ok(true)
-                }
-            });
+        self.out_visitor_trait.extend(quote! {
+            #[inline]
+            fn #pre(&mut self, val: &mut #name) -> crate::error::Result<bool> {
+                Ok(true)
+            }
+        });
         Ok(())
     }
 
     fn add_visitor_post(&mut self, name: &Ident) -> syn::Result<()> {
         let lower_name = name.to_string().to_lowercase();
         let post = Ident::new(&format!("{lower_name}_post"), name.span());
-        self.out_visitor_trait
-            .extend(quote! {
-                #[inline]
-                fn #post(&mut self, val: &mut #name) -> Result<()> {
-                    Ok(())
-                }
-            });
+        self.out_visitor_trait.extend(quote! {
+            #[inline]
+            fn #post(&mut self, val: &mut #name) -> Result<()> {
+                Ok(())
+            }
+        });
         Ok(())
     }
 
@@ -351,29 +366,145 @@ impl AstGenerator {
     }
 }
 
-// Return the lower-case version of an identifier.
+/// Build code to recurse into the given fields. If unnamed, they are assumed to have names v0,
+/// v1, ..
+fn recurse_for_fields<F: FnMut(&Ident) -> proc_macro2::TokenStream>(
+    fields: &Fields,
+    mut val_expr: F,
+) -> syn::Result<proc_macro2::TokenStream> {
+    let mut result = proc_macro2::TokenStream::new();
+    match fields {
+        Fields::Named(fields) => {
+            for field in &fields.named {
+                result.extend(recurse_for_value(
+                    val_expr(field.ident.as_ref().unwrap()),
+                    &field.ty,
+                )?);
+            }
+        }
+        Fields::Unnamed(fields) => {
+            for (i, field) in fields.unnamed.iter().enumerate() {
+                let name = Ident::new(&format!("v{i}"), field.ty.span());
+                result.extend(recurse_for_value(val_expr(&name), &field.ty)?);
+            }
+        }
+        Fields::Unit => {}
+    }
+    Ok(result)
+}
+
+/// Build code to recurse for a single value. This is where we special-case all of the Node
+/// types and variants.
+fn recurse_for_value(
+    value: proc_macro2::TokenStream,
+    ty: &Type,
+) -> syn::Result<proc_macro2::TokenStream> {
+    Ok(match TraversableType::from_type(ty) {
+        Some(TraversableType::NodeK) | Some(TraversableType::NodeRefK) => {
+            quote! { #value.traverse(self)?; }
+        }
+        Some(TraversableType::OptionNodeK) | Some(TraversableType::OptionNodeRefK) => {
+            quote! { if let Some(v) = #value { v.traverse(self)?; } }
+        }
+        Some(TraversableType::VecNodeK) | Some(TraversableType::VecNodeRefK) => {
+            quote! { for v in #value { v.traverse(self)?; } }
+        }
+        None => {
+            // Not recursible.
+            Default::default()
+        }
+    })
+}
+
+/// The various types we support traversing.
+enum TraversableType {
+    /// Node<..>
+    NodeK,
+    /// NodeRef<..>
+    NodeRefK,
+    /// Option<Node<..>>
+    OptionNodeK,
+    /// Option<NodeRef<..>>
+    OptionNodeRefK,
+    /// Vec<Node<..>>
+    VecNodeK,
+    /// Vec<NodeRef<..>>
+    VecNodeRefK,
+}
+
+impl TraversableType {
+    fn from_type(ty: &Type) -> Option<Self> {
+        let Type::Path(ty) = ty else {
+            return None;
+        };
+        let path = &ty.path;
+        if path.leading_colon.is_some() || path.segments.len() != 1 {
+            return None;
+        }
+        let type_name = &path.segments[0].ident;
+        if type_name == "Node" {
+            Some(TraversableType::NodeK)
+        } else if type_name == "NodeRef" {
+            Some(TraversableType::NodeRefK)
+        } else if type_name == "Option" || type_name == "Vec" {
+            let PathArguments::AngleBracketed(arguments) = &path.segments[0].arguments else {
+                return None;
+            };
+            if arguments.args.len() != 1 {
+                return None;
+            }
+            let GenericArgument::Type(ty) = &arguments.args[0] else {
+                return None;
+            };
+            match TraversableType::from_type(ty) {
+                Some(TraversableType::NodeK) if type_name == "Option" => {
+                    Some(TraversableType::OptionNodeK)
+                }
+                Some(TraversableType::NodeRefK) if type_name == "Option" => {
+                    Some(TraversableType::OptionNodeRefK)
+                }
+                Some(TraversableType::NodeK) if type_name == "Vec" => {
+                    Some(TraversableType::VecNodeK)
+                }
+                Some(TraversableType::NodeRefK) if type_name == "Vec" => {
+                    Some(TraversableType::VecNodeRefK)
+                }
+                _ => None,
+            }
+        } else {
+            None
+        }
+    }
+}
+
+/// Create a `mut ref name` PatIdent.
+fn pat_ident(name: Ident) -> PatIdent {
+    PatIdent {
+        attrs: Vec::new(),
+        by_ref: None,
+        mutability: None,
+        ident: name,
+        subpat: None,
+    }
+}
+
+/// Return the lower-case version of an identifier.
 fn lowercase(ident: &Ident) -> Ident {
     let name = ident.to_string().to_lowercase();
     Ident::new(&name, ident.span())
 }
 
-// Given a field, make a FnArg for it using `impl Into<f.ty>`.
+/// Given a field, make a FnArg for it using `impl Into<f.ty>`.
 fn fn_arg_impl_into(f: &Field) -> FnArg {
     FnArg::Typed(PatType {
         attrs: Vec::new(),
-        pat: Box::new(Pat::Ident(PatIdent {
-            attrs: Vec::new(),
-            by_ref: None,
-            mutability: None,
-            ident: f.ident.clone().unwrap(),
-            subpat: None,
-        })),
+        pat: Box::new(Pat::Ident(pat_ident(f.ident.clone().unwrap()))),
         colon_token: Token![:](f.span()),
         ty: Box::new(impl_into(&f.ty)),
     })
 }
 
-// Turn `ty` into `impl Into<ty>`.
+/// Turn `ty` into `impl Into<ty>`.
 fn impl_into(ty: &Type) -> Type {
     Type::ImplTrait(TypeImplTrait {
         impl_token: Token![impl](ty.span()),
